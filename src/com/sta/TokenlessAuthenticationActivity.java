f13cb647e4c6179f03507aaf6867ac24ade38185
Robert Khasanov <rob.khasanov@gmail.com>
2011-10-31 07:21:45 +0400

    Added simple work with files.
    Signed-off-by: Robert Khasanov <rob.khasanov@gmail.com>

--- src/com/sta/TokenlessAuthenticationActivity.java
+++ src/com/sta/TokenlessAuthenticationActivity.java
@@ -1,14 +1,91 @@
 package com.sta;
 
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 public class TokenlessAuthenticationActivity extends Activity {
+	public static final int CONNECT_ID = Menu.FIRST;
+	public static final int WRITE_ID = Menu.FIRST + 1;
+	public static final int READ_ID = Menu.FIRST + 2;
+
+	private EditText mBodyText;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         setTitle( R.string.app_name);
+        mBodyText = (EditText) findViewById( R.id.body);
     }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	boolean result = super.onCreateOptionsMenu(menu);
+        menu.add(0, CONNECT_ID, 0, R.string.connect);
+        menu.add(0, WRITE_ID, 0, R.string.write);
+        menu.add(0, READ_ID, 0, R.string.read);
+        
+        return result;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	try {
+    	switch (item.getItemId()) {
+        case CONNECT_ID:
+            connect_to_server();
+            return true;
+        case WRITE_ID:
+        	writeFile("aaas");
+        	return true;
+        case READ_ID:
+        	readFile("aaas");
+        }
+    	} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void connect_to_server(){
+    	return;
+    }
+    
+    private void writeFile(String path) throws IOException{
+        String body = mBodyText.getText().toString();
+    	File file = new File(path);
+    	file.createNewFile();
+        FileOutputStream fOut = openFileOutput(file.getAbsolutePath(), MODE_WORLD_READABLE);
+        OutputStreamWriter osw = new OutputStreamWriter(fOut);
+        osw.write(body);
+        osw.flush();
+        osw.close();
+    	return;
+    }
+    
+    private void readFile( String path) throws IOException{
+       	File file = new File(path);
+       	if ( file.exists()) {
+            FileInputStream fIn = openFileInput(file.getAbsolutePath());
+            InputStreamReader isr = new InputStreamReader(fIn);
+            char[] inputBuffer = new char[1000];
+            isr.read(inputBuffer);
+            String read = new String(inputBuffer);
+        	mBodyText.setText( read);
+      	}
+    }
+    
 }
