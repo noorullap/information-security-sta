6043554da237007a6257e2cd249ccbdd3b15acdf
Vasily Oreshko <vasily.oreshko@gmail.com>
2011-11-03 18:54:44 +0400

    Probe

--- src/com/sta/TokenlessAuthenticationActivity.java
+++ src/com/sta/TokenlessAuthenticationActivity.java
@@ -23,6 +23,7 @@ public class TokenlessAuthenticationActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_win);
         setTitle( R.string.app_name);
